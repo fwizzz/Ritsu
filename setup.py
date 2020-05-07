b15358f6bef4c891b3bd05ce28994ed136f70f47
Faiz Rahim <50950705+fwizzz@users.noreply.github.com>
2020-05-07 23:22:03 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
-reddit_client_id = "EvLPVk7GCoVgrQ"
-reddit_client_secret = "Y4NleCG1wpxY1Td_im812-zWIBM"
-reddit_username='pyUsagi'
+reddit_client_id = ""
+reddit_client_secret = ""
+reddit_username=''
 
-discord_bot_token = "NTc3MTQwMTc4NzkxOTU2NTAw.XrFw8A.Cc6IJ3pxUnXxUIrpe1BWCBMBF1M"
+discord_bot_token = ""
