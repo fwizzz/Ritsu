4570a5785b9b482d5f4ccd07632e705dd38f2c25
Faiz Rahim <50950705+fwizzz@users.noreply.github.com>
2020-05-15 22:06:24 +0530

    Update processing.py

--- cogs/utils/processing.py
+++ cogs/utils/processing.py
@@ -12,12 +12,16 @@ def split_num(num):
 def mapnum(a, b):
     stopmodifed = (b // 1000 - 1) * 1000
     b -= stopmodifed
+
     if len(split_num(a)) > 2:
-       a = split_num(a)[2]
+        a = split_num(a)[2]
     else:
-       pass
-    return a,b
+        pass
+    return a, b
 
 
-
-print(split_num(40))
+def volumeBar(count, total, bar_len=10):
+    filled_len = int(round(bar_len * count / float(total)))
+    percents = round(100.0 * count / float(total), 1)
+    bar = '█' * filled_len + '░' * (bar_len - filled_len)
+    return '%s' % (bar)
